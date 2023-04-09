5de39c06adbd1ee6aa0407379b1e35fbec8cd661
Joul Qasis <Joulstudying@hotmail.com>
2023-04-09 14:34:50 +0200

    deployment

--- app.js
+++ app.js
@@ -6,13 +6,21 @@ const cors = require('cors')
 require('dotenv').config();
 // functionality of the app
 const app = express();
-app.use(cors({ credentials: true, origin: "http://localhost:3000" }));
+app.use(cors({ credentials: true, origin: ["http://localhost:3000","https://brain-rush-f-rgo4-r15jna2dq-joulqasis.vercel.app/"] }));
 app.use(cookieParser());
 app.use(express.json());
+
+// add the following middleware to set the Access-Control-Allow-Origin header
+app.use((req, res, next) => {
+  res.header('Access-Control-Allow-Origin', '*');
+  res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
+  res.header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE');
+  next();
+});
+
 app.use('/api', router);
 
-// password  KVMPKJFfq8KwvDBb
-// connect to database if successfull app.listen port 5000!
+// connect to database if successful app.listen port 5000!
 mongoose.connect(`mongodb+srv://BrainRushGames:${process.env.MONGODB_PASSWORD}@brainrush-cluster.4qfwa0t.mongodb.net/?retryWrites=true&w=majority`)
     .then(() => {
         app.listen(5000);
@@ -21,5 +29,3 @@ mongoose.connect(`mongodb+srv://BrainRushGames:${process.env.MONGODB_PASSWORD}@b
 // middlewear
 
 module.exports = router;
-
-
